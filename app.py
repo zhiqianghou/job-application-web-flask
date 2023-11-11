7da58fb9b6f59e8b6d4eabef028a84374f2c4a45
zhiqianghou <zhiqiang.hou.ibp@gmail.com>
2023-11-11 09:16:20 -0600

    Add submission notification module

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, flash
 from flask_sqlalchemy import SQLAlchemy  #pip install flask-sqlalchemy
 
 
@@ -34,6 +34,7 @@ def index():
 					email=email, date=date, occupation=occupation)
 		db.session.add(form)
 		db.session.commit()
+		flash(f"{first_name},Your form was submitted successfully!", "success")
 
 
 	return render_template("index.html")
